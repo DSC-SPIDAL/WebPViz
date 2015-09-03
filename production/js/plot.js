04620e99dff21987cb0c118e78a261efcfe421df
pulasthi <pulasthi911@gmail.com>
2015-09-03 16:45:34 -0400

    generating checkboxlist

---
+++ production/js/plot.js
@@ -0,0 +1,34 @@
+function generateCheckList(){
+
+    var tabletop = "<table class='table table-striped' style='background-color: #EAEAEA; padding-top: 0px'>"
+                       + "<thead>"
+                           + "<tr class='headings'>"
+                           +     "<th>"
+                           +     "<input type='checkbox' id='check-all' disabled='true' class='flat'> Cluster"
+                           +     "</th>"
+                           +     "<th class='column-title'>Label</th>"
+                           +     "<th class='column-title'>Size</th>"
+                           +     "</th>"
+                       +     "</tr>"
+                       + "</thead>"
+                       +"<tbody>"
+
+
+
+
+
+
+}
+/*
+<thead>
+<tr class="headings">
+    <th>
+    <input type="checkbox" id="check-all" disabled="true" class="flat"> Cluster
+    </th>
+    <th class="column-title">Label</th>
+    <th class="column-title">Size</th>
+    </th>
+    </tr>
+    </thead>
+
+    */
