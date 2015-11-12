a5dfd351564625f6a7cf241cd42e9239ad58238e
pulasthi <pulasthi911@gmail.com>
2015-11-11 20:48:22 -0500

    adding control panel

--- public/javascripts/plot.js
+++ public/javascripts/plot.js
@@ -247,6 +247,7 @@ function generateGraph() {
         // document.getElementById("stats").appendChild(stats.domElement);
         window.addEventListener('resize', onWindowResize, true);
         $('.color-pic1').colorpicker();
+        setupGui();
         render();
         animate();
     });
@@ -786,4 +787,107 @@ function resetSlider() {
     //$("#plot-slider").attr("value", 0);
     plotRangeSlider.update({from: 0});
     updatePlot(0);
+}
+
+var params = {
+    interation: 5000
+};
+
+
+
+function setupGui() {
+
+    effectController = {
+
+        shininess: 40.0,
+        ka: 0.17,
+        kd: 0.51,
+        ks: 0.2,
+        metallic: true,
+
+        hue:		0.121,
+        saturation: 0.73,
+        lightness:  0.66,
+
+        lhue:		 0.04,
+        lsaturation: 0.01,	// non-zero so that fractions will be shown
+        llightness:  1.0,
+
+        // bizarrely, if you initialize these with negative numbers, the sliders
+        // will not show any decimal places.
+        lx: 0.32,
+        ly: 0.39,
+        lz: 0.7,
+        newTess: 15,
+        bottom: true,
+        lid: true,
+        body: true,
+        fitLid: false,
+        nonblinn: false,
+        newShading: "glossy"
+    };
+
+    var h;
+
+    var gui = new dat.GUI({ autoPlace: false });
+    var customContainer = document.getElementById('plot-controls');
+    customContainer.appendChild(gui.domElement);
+
+    //gui.add(text, 'message');
+    //gui.add(text, 'speed', -5, 5);
+    //gui.add(text, 'displayOutline');
+    //gui.add(text, 'explode');
+
+
+     //material (attributes)
+    gui.add(params, 'interation')
+    gui.add(params, 'interation')
+    gui.add(params, 'interation')
+    gui.add(params, 'interation')
+    //h.add( effectController, "shininess", 1.0, 400.0, 1.0 ).name( "shininess" ).onChange( render );
+
+
+    //h = gui.addFolder( "Material control" );
+        //
+        //h.add( effectController, "shininess", 1.0, 400.0, 1.0 ).name( "shininess" ).onChange( render );
+        //h.add( effectController, "kd", 0.0, 1.0, 0.025 ).name( "diffuse strength" ).onChange( render );
+        //h.add( effectController, "ks", 0.0, 1.0, 0.025 ).name( "specular strength" ).onChange( render );
+        //h.add( effectController, "metallic" ).onChange( render );
+        //
+        //// material (color)
+        //
+        //h = gui.addFolder( "Material color" );
+        //
+        //h.add( effectController, "hue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
+        //h.add( effectController, "saturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
+        //h.add( effectController, "lightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
+        //
+        //// light (point)
+        //
+        //h = gui.addFolder( "Lighting" );
+        //
+        //h.add( effectController, "lhue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
+        //h.add( effectController, "lsaturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
+        //h.add( effectController, "llightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
+        //h.add( effectController, "ka", 0.0, 1.0, 0.025 ).name( "ambient" ).onChange( render );
+        //
+        //// light (directional)
+        //
+        //h = gui.addFolder( "Light direction" );
+        //
+        //h.add( effectController, "lx", -1.0, 1.0, 0.025 ).name( "x" ).onChange( render );
+        //h.add( effectController, "ly", -1.0, 1.0, 0.025 ).name( "y" ).onChange( render );
+        //h.add( effectController, "lz", -1.0, 1.0, 0.025 ).name( "z" ).onChange( render );
+        //
+        //h = gui.addFolder( "Tessellation control" );
+        //h.add( effectController, "newTess", [ 2, 3, 4, 5, 6, 8, 10, 15, 20, 30, 40, 50 ] ).name( "Tessellation Level" ).onChange( render );
+        //h.add( effectController, "lid" ).name( "display lid" ).onChange( render );
+        //h.add( effectController, "body" ).name( "display body" ).onChange( render );
+        //h.add( effectController, "bottom" ).name( "display bottom" ).onChange( render );
+        //h.add( effectController, "fitLid" ).name( "snug lid" ).onChange( render );
+        //h.add( effectController, "nonblinn" ).name( "original scale" ).onChange( render );
+        //
+        //// shading
+        //h = gui.add( effectController, "newShading", [ "wireframe", "flat", "smooth", "glossy", "textured", "reflective" ] ).name( "Shading" ).onChange( render );
+
 }
