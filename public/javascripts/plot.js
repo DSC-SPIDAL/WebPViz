9aae711a5e74abb94bbd8de6c5bedf82f77a7bf6
pulasthi <pulasthi911@gmail.com>
2015-12-20 17:31:08 -0500

    adding changes for glyp selection

--- public/javascripts/plot.js
+++ public/javascripts/plot.js
@@ -104,33 +104,8 @@ function generateClusterList(list, initcolors) {
             var key = nonEmptyList[i];
             if (list.hasOwnProperty(key)) {
                 var colorWithouthHash = initcolors[key].replace(/#/g, '')
-                if (list[key].size > 1) {
-                    var sprite;
-                    switch (parseInt(list[key].shape)) {
-                        case 0:
-                            sprite = "Disc";
-                            break;
-                        case 1:
-                            sprite = "Ball";
-                            break;
-                        case 2:
-                            sprite = "Star";
-                            break;
-                        case 3:
-                            sprite = "Cube";
-                            break;
-                        case 4:
-                            sprite = "Pyramid";
-                            break;
-                        case 5:
-                            sprite = "Cone";
-                            break;
-                        case 6:
-                            sprite = "Cylinder";
-                            break;
-                        default :
-                            sprite = "Cube";
-                    }
+                var sprite = getGlyphName(list[key]);
+                if (sprite != null ) {
                     grid += "<div class='element-item transition metal' data-category='transition' style='background-color: #" + colorWithouthHash + " '>" +
                         "<p style='font-size: 0.8em'><span style='font-weight: bold'>" + list[key].label + "(" + sprite + ")" + "</span>:" + list[key].length + "</p></div>"
                 } else {
@@ -143,6 +118,38 @@ function generateClusterList(list, initcolors) {
     }
 
     return grid;
+}
+
+function getGlyphName(key){
+    var glyph = null;
+    if (key.size > 1) {
+        switch (parseInt(key.shape)) {
+            case 0:
+                glyph = "Disc";
+                break;
+            case 1:
+                glyph = "Ball";
+                break;
+            case 2:
+                glyph = "Star";
+                break;
+            case 3:
+                glyph = "Cube";
+                break;
+            case 4:
+                glyph = "Pyramid";
+                break;
+            case 5:
+                glyph = "Cone";
+                break;
+            case 6:
+                glyph = "Cylinder";
+                break;
+            default :
+                glyph = "Cube";
+        }
+    }
+    return glyph;
 }
 
 function colorEnable(check) {
@@ -200,15 +207,20 @@ function generateCheckList(list, initcolors) {
             }else{
                 tablerows += "<input type='checkbox' class='flat' name='table_records' value='" + key + "'>";
             }
+            var sprite = getGlyphName(list[key]);
             tablerows += "<label class='color-box-label'>" + key + "</label> "
                 + "<div class='input-group color-pic1' style='width: 15px;height: 15px; display: inline-flex; padding-left: 20px;padding-top: 2px'>"
                 + "<input value='" + initcolors[key] + "' class='form-control' type='hidden' id='" + key + "'>"
                 + "<span class='input-group-addon color-picker-addon'><i style='background-color: rgb(1, 343, 69);'></i></span>"
                 + "</div>"
-                + "</td>"
-                + "<td class=' '>" + list[key].label + "</span></td>"
-                + "<td class='l1'>" + list[key].length + "</td>"
-                + "</tr>";
+                + "</td>";
+            if(sprite != null){
+                tablerows += "<td class=' '>" + list[key].label + sprite + "</span></td>";
+            }else{
+                tablerows += "<td class=' '>" + list[key].label +"</span></td>";
+            }
+                tablerows += "<td class='l1'>" + list[key].length + "</td>"
+                    + "</tr>";
         //}
     }
 
