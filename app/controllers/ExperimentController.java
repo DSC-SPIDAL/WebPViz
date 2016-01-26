f988f6f7cad5b49f7b15e766df4680a66b0ae2ca
supunkamburugamuva <supun06@gmail.com>
2016-01-26 17:36:06 -0500

    experiment controller implementation

--- app/controllers/ExperimentController.java
+++ app/controllers/ExperimentController.java
@@ -1,7 +1,39 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import db.Constants;
+import db.ExperimentDAO;
+import models.User;
+import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security;
 
 public class ExperimentController extends Controller {
+    @Security.Authenticated(Secured.class)
+    public static Result getExperiment(int artifactId) {
+        User loggedInUser = User.findByEmail(request().username());
+        String comment = ExperimentDAO.getExperiment(artifactId, loggedInUser.email);
+        if (comment != null) {
+            return ok("{status: 'success'}").as("application/json");
+        } else {
+            return badRequest("{status: 'fail'}").as("application/json");
+        }
+    }
 
+    @Security.Authenticated(Secured.class)
+    public static Result updateExperiment() {
+        User loggedInUser = User.findByEmail(request().username());
+        JsonNode json = request().body().asJson();
+        String body = Json.stringify(json);
+        int timeSeriesId = 0;
+        String timeSeries = json.get(Constants.Experiment.TIME_SERIES_ID_FIELD).asText();
+        if (timeSeries != null) {
+            timeSeriesId = Integer.parseInt(timeSeries);
+            ExperimentDAO.updateExperiment(timeSeriesId, loggedInUser.email, body);
+            return ok("{status: 'success'}").as("application/json");
+        } else {
+            return badRequest("{status: 'fail'}").as("application/json");
+        }
+    }
 }
