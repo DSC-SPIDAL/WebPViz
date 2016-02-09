e64002632a6dac62466cfe180ab49753dce74069
pulasthi <pulasthi911@gmail.com>
2016-02-09 13:15:45 -0500

    adding search by tag

--- app/db/SearchDAO.java
+++ app/db/SearchDAO.java
@@ -2,11 +2,21 @@ package db;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.QueryBuilder;
 import com.mongodb.client.FindIterable;
 import models.Tag;
+import models.TimeSeries;
 import org.bson.Document;
+import org.h2.command.dml.Query;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchDAO {
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static String getArtifactsByTag(String user,String tagname){
         MongoConnection db = MongoConnection.getInstance();
@@ -14,16 +24,53 @@ public class SearchDAO {
         document.append(Constants.Tags.TAGS_FIELD + "." + tagname + "." + Constants.Tags.NAME,tagname);
         FindIterable<Document> iterable = db.plotTagsCol.find(document);
 
-        JsonArray tidarray = new JsonArray();
-        int tagcount = 1;
+        ArrayList<Integer> tidarray = new  ArrayList<Integer>();
         for (Document d : iterable) {
-            String category = (String) d.get(Constants.Tags.CATEGORY);
-            JsonObject temp = new JsonObject();
-            temp.addProperty("category",category);
-            tidarray.add(temp);
-            tagcount++;
+            Integer tid = (Integer) d.get(Constants.Tags.TIME_SERIES_ID_FIELD);
+            tidarray.add(tid);
         }
+
+        BasicDBObject obj = new BasicDBObject (Constants.Artifact.ID_FIELD, new BasicDBObject("$in", tidarray));
+        FindIterable<Document> iterableTimesSeries = db.artifactCol.find(obj);
+        List<TimeSeries> timeSeriesList = getTimeSeriesList(iterableTimesSeries);
         return tidarray.toString();
+    }
+
+    public static List<TimeSeries> getTimeSeriesList(FindIterable<Document> iterable) {
+        List<TimeSeries> timeSeriesList = new ArrayList<TimeSeries>();
+        for (Document document : iterable) {
+            TimeSeries timeSeries = new TimeSeries();
+            try {
+                timeSeries.dateCreation = format.parse((String) document.get(Constants.Artifact.DATE_CREATION_FIELD));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            timeSeries.id = (Integer) document.get(Constants.Artifact.ID_FIELD);
+            timeSeries.name = (String) document.get(Constants.Artifact.NAME_FIELD);
+            timeSeries.description = (String) document.get(Constants.Artifact.DESC_FIELD);
+            timeSeries.uploaderId = (String) document.get(Constants.Artifact.USER);
+            timeSeries.status = (String) document.get(Constants.Artifact.STATUS_FIELD);
+            timeSeries.group = (String) document.get(Constants.Artifact.GROUP_FIELD);
+            Object pub = document.get(Constants.Artifact.PUBLIC);
+            if (pub != null && pub instanceof Boolean) {
+                timeSeries.pub = (boolean) pub;
+            }
+            if (timeSeries.group == null || "".equals(timeSeries.group)) {
+                timeSeries.group = Constants.Group.DEFAULT_GROUP;
+            }
+            Object resultSetsObject = document.get(Constants.Artifact.FILES);
+            if (resultSetsObject != null && resultSetsObject instanceof List) {
+                if (((List)resultSetsObject).size() > 1) {
+                    timeSeries.t = "T";
+                } else {
+                    timeSeries.t = "S";
+                }
+            } else {
+                timeSeries.t = "S";
+            }
+            timeSeriesList.add(timeSeries);
+        }
+        return timeSeriesList;
     }
 
 }
