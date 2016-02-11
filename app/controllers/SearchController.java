08e40eab314dcdad8fcd21f316caf0b76097de7c
pulasthi <pulasthi911@gmail.com>
2016-02-11 15:25:52 -0500

    tag search working

--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -4,11 +4,13 @@ package controllers;
 import db.GroupsDAO;
 import db.SearchDAO;
 import db.TagsDAO;
+import models.Group;
 import models.TimeSeries;
 import models.User;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import views.html.dashboard;
 import views.html.tagsearch;
 
 import java.util.List;
@@ -17,10 +19,10 @@ public class SearchController extends Controller {
     @Security.Authenticated(Secured.class)
     public static Result searchArtifactsByTag(String tagname) {
         User loggedInUser = User.findByEmail(request().username());
-        List<TimeSeries> tags = SearchDAO.getArtifactsByTag(loggedInUser.email,tagname);
+        List<TimeSeries> taggedtimeseries = SearchDAO.getArtifactsByTag(loggedInUser.email,tagname);
         String[] tagslist = {tagname};
-        if (tags != null) {
-            return ok(tagsearch.render(loggedInUser, false, null, tagslist, tags));
+        if (taggedtimeseries != null) {
+            return ok(dashboard.render(loggedInUser, false, null, taggedtimeseries, GroupsDAO.allGroups(loggedInUser.email), false, true, tagslist, null, false, "Dashboard"));
         } else {
             return badRequest("{status: 'fail'}").as("application/json");
         }
