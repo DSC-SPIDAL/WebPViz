555eb193527658c9cc5c138fdd57fe37cea6d344
pulasthi <pulasthi911@gmail.com>
2016-05-12 15:30:46 -0400

    adding getter setters for signup class:

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -496,6 +496,22 @@ public class Application extends Controller {
         @Constraints.Required
         public String password;
 
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
         /**
          * Validate the authentication.
          *
